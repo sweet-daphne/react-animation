9d9063a78b76fd21c6d47f29742acd86b6edc566
suelrin <dancerelrin@gmail.com>
2023-05-26 07:03:44 +0900

    gestures

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,8 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
+/* variants
+  const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   display: grid;
@@ -66,6 +67,41 @@ function App() {
         <Circle variants={circleVariants} />
         <Circle variants={circleVariants} />
       </Box>
+    </Wrapper>
+  );
+}
+*/
+
+const Box = styled(motion.div)`
+  width: 200px;
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const boxVariants = {
+  hover: {
+    scale: 1.5,
+    rotateZ: 90,
+  },
+  click: {
+    scale: 1,
+    borderRadius: '100px',
+  },
+  drag: { backgroundColor: 'rgba(46,204,113)', transition: { duration: 10 } },
+};
+
+function App() {
+  return (
+    <Wrapper>
+      <Box
+        drag
+        variants={boxVariants}
+        whileHover="hover"
+        whileDrag="drag"
+        whileTap="click"
+      />
     </Wrapper>
   );
 }
